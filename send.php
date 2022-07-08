fe0b8d618d5ead741c19aa5aa2629ec9a5d879cf
Dmitrij413 <106316147+Dmitrij413@users.noreply.github.com>
2022-07-08 11:32:21 +0400

    add send.php

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("dmitrij.khramov@bk.ru", "Сообщение с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: git@github.com:Dmitrij413/resume.git \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
